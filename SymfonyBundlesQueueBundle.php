a5a8340f4c37aaaed1a2e4495a738294dc746534
Dmitry Khaperets <khaperets@gmail.com>
2016-06-02 06:10:12 +0300

    Added a bundle dependency

--- SymfonyBundlesQueueBundle.php
+++ SymfonyBundlesQueueBundle.php
@@ -3,9 +3,13 @@
 namespace SymfonyBundles\QueueBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use SymfonyBundles\BundleDependency\BundleDependency;
+use SymfonyBundles\BundleDependency\BundleDependencyInterface;
 
-class SymfonyBundlesQueueBundle extends Bundle
+class SymfonyBundlesQueueBundle extends Bundle implements BundleDependencyInterface
 {
+
+    use BundleDependency;
 
     /**
      * {@inheritdoc}
@@ -13,6 +17,16 @@ class SymfonyBundlesQueueBundle extends Bundle
     public function getContainerExtension()
     {
         return new DependencyInjection\QueueExtension();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundleDependencies()
+    {
+        return [
+            \SymfonyBundles\RedisBundle\SymfonyBundlesRedisBundle::class
+        ];
     }
 
 }
